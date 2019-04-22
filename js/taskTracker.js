7f0b1a283f25b4e16982028280c0712618350630
NikitaSysoev <na.sysoev@gmail.com>
2019-04-23 01:50:24 +0300

    add tasks1-2 in homework4

--- js/taskTracker.js
+++ js/taskTracker.js
@@ -1,11 +1,12 @@
 const STATE = {
   taskList: [], // список задач
   formState: "add", // [ add, edit, err ] - состсояние формы
+  editIndex: null,
+  modalBody: null,
+  modalType: null,
   isOpenedPopup: false, // true / false -флаг открыто/ закрыто модальное окно
   formData: {
     // данные формы
-    id: null, // уникальный идентификатор
-    index: null, // индекс редактируемого элемента
     taskName: null, // название задачи
     taskDescription: null, // описание
     taskDate: null, // дата
@@ -14,8 +15,6 @@ const STATE = {
 };
 
 function collectDataFromForm() {
-  // создаем уникальный идентификатор для кажого вновь созданнного таска
-  STATE.formData.id = "id" + new Date().getTime();
   STATE.formData.taskName = document.getElementById("taskName").value;
   STATE.formData.taskDescription = document.getElementById(
     "taskDescription"
@@ -43,18 +42,13 @@ function fillForm({ taskName, taskDescription, taskDate, taskUrgent }) {
 }
 
 function pushDataToList() {
-  if (STATE.formState === "add") {
-    STATE.taskList = [...STATE.taskList, STATE.formData];
-  }
   if (STATE.formState === "edit") {
-    const newArrayTaskList = [...STATE.taskList];
-    newArrayTaskList.splice(STATE.formData.index, 1, STATE.formData);
-    STATE.taskList = newArrayTaskList;
+    STATE.taskList[STATE.editIndex] = STATE.formData;
+  } else {
+    STATE.taskList.push(STATE.formData);
   }
   STATE.formState = "add";
   STATE.formData = {
-    id: null,
-    index: null,
     taskName: null,
     taskDescription: null,
     taskDate: null,
@@ -73,74 +67,105 @@ function validateFromForm() {
   return retVal;
 }
 
-function storageToState() {
-  const localList = JSON.parse(localStorage.getItem("taksList"));
-  STATE.taskList = Array.isArray(localList) ? localList : [];
+function initPage() {
+  let localList;
+  try {
+    localList = JSON.parse(localStorage.getItem("TASKS"));
+  } catch (e) {
+    log("Coudnt init JSON from Local Storage", e);
+  }
+  STATE.taskList = localList || [];
+  renderList();
 }
 
 function stateToStorage() {
-  localStorage.setItem("taksList", JSON.stringify(STATE.taskList));
+  localStorage.setItem("TASKS", JSON.stringify(STATE.taskList));
+  return true;
 }
 
-function clearList() {
+function handleClearList() {
   STATE.taskList = [];
-  localStorage.removeItem("taksList");
+  localStorage.removeItem("TASKS");
   renderList();
+  clearForm();
+  STATE.formState = "add";
+  STATE.formData = {
+    taskName: null,
+    taskDescription: null,
+    taskDate: null,
+    taskUrgent: false
+  };
+  renderButton();
 }
 
 function renderList() {
   const container = document.getElementById("taksList");
   let div = "";
-  STATE.taskList.forEach(item => {
-    div += `<li id="${item.id}" class="list-group-item">`;
-    div += `<p>${item.taskName} `;
+  STATE.taskList.forEach((item, index) => {
+    div += `<li class="list-group-item">`;
+    div += `<a onclick="handleTaskInfo(event)" href="#"><p>${
+      item.taskName
+    }</p></a>`;
     div += item.taskUrgent
       ? '<i class="text-danger fas fa-exclamation-triangle"></i> '
       : "";
-    div += "</p>";
     div += `<p>${item.taskDescription}</p><p>${item.taskDate}</p>`;
-    div += '<i class="fas fa-edit" style="cursor: pointer"></i> ';
-    div += '<i class="far fa-trash-alt" style="cursor: pointer"></i>';
+    div += `<i data-id=${index} onClick="handleEditTask(event)" class="fas fa-edit" style="cursor: pointer"></i> `;
+    div += `<i data-id=${index} onClick="handleDeleteTask(event)" class="far fa-trash-alt" style="cursor: pointer"></i>`;
     div += "</li>";
   });
   div +=
-    '<button class="btn btn-danger" onclick="clearList();" style="margin-top: 10px;">';
+    '<button class="btn btn-danger" onclick="handleClearList();" style="margin-top: 10px;">';
   div += "Clear List";
   div += "</button>";
-  container.innerHTML = STATE.taskList.length ? div : "";
+  const noDiv =
+    "<li class='list-group-item'><strong class='text-secondary'>Список пуст</strong></li>";
+  container.innerHTML = STATE.taskList.length ? div : noDiv;
+}
+
+function handleClearForm() {
+  if (STATE.formState === "edit") {
+    STATE.formState = "add";
+  }
+  clearForm();
+  renderButton();
 }
 
 function renderButton() {
   const button = document.getElementById("clickTask");
-  if (STATE.formState === "add") {
-    button.classList.remove("btn-warning");
-    button.classList.add("btn-primary");
-    button.textContent = "Add new task";
-  }
+  const button2 = document.getElementById("clearCancel");
+  const header = document.getElementById("form-header");
   if (STATE.formState === "edit") {
     button.classList.remove("btn-primary");
     button.classList.add("btn-warning");
     button.textContent = "Edit task";
+    button2.textContent = "Cancel";
+    header.textContent = "Edit Task";
+  } else {
+    button.classList.remove("btn-warning");
+    button.classList.add("btn-primary");
+    button.textContent = "Add new task";
+    button2.textContent = "Clear form";
+    header.textContent = "Add Task";
   }
 }
 
 function handleDeleteTask(e) {
   STATE.formState = "add";
-  const id = e.target.parentNode.id;
-  STATE.taskList = STATE.taskList.filter(item => item.id !== id);
+  const index = e.target.getAttribute("data-id");
+  STATE.taskList.splice(index, 1);
   renderAndSave();
 }
 
 function handleEditTask(e) {
   if (STATE.formState === "add") {
     STATE.formState = "edit";
-    const id = e.target.parentNode.id;
-    const findedItem = STATE.taskList.find(item => item.id === id);
-    STATE.formData.index = STATE.taskList.indexOf(findedItem);
-    fillForm(findedItem);
+    const index = e.target.getAttribute("data-id");
+    fillForm(STATE.taskList[index]);
+    STATE.editIndex = index;
   } else {
     STATE.formState = "add";
-    STATE.formData.index = null;
+    STATE.editIndex = null;
     clearForm();
   }
   renderButton();
@@ -175,6 +200,54 @@ function handleTask() {
     pushDataToList();
     renderAndSave();
   }
+}
+
+function handleTaskInfo(e) {
+  e.preventDefault();
+  const index = e.currentTarget.parentNode
+    .getElementsByClassName("fa-trash-alt")[0]
+    .getAttribute("data-id");
+  STATE.modalType = "INFO";
+  STATE.modalBody = STATE.taskList[index];
+  renderModalWindow();
+}
+
+function renderModalWindow() {
+  const modal = document.getElementById("modalWindow");
+  modal.innerHTML = "";
+  let div = "";
+  div += '<div class="modal-dialog" role="document">';
+  div += '<div class="modal-content">';
+  div += '<div class="modal-header">';
+  div += `<h5 class="modal-title">${STATE.modalType}</h5>`;
+  div += '<button type="button" onclick="handleCloseModal();" class="close">';
+  div += '<span aria-hidden="true">&times;</span>';
+  div += '</button></div><div class="modal-body">';
+  if (STATE.modalType === "INFO") {
+    div += `<p>Task Name - ${STATE.modalBody.taskName}</p>`;
+    div += STATE.modalBody.taskDescription
+      ? `<p>Task Description - ${STATE.modalBody.taskDescription}</p>`
+      : "";
+    div += `<p>Task Date - ${STATE.modalBody.taskDate}</p>`;
+    div += STATE.modalBody.taskUrgent
+      ? `<i class="text-danger fas fa-exclamation-triangle"></i>`
+      : "";
+  }
+  div += '</div><div class="modal-footer">';
+  div +=
+    '<button type="button" class="btn btn-secondary" onclick="handleCloseModal();" data-dismiss="modal">';
+  div += "Close";
+  div += "</button>";
+  div += '<button type="button" class="btn btn-primary">Save changes</button>';
+  div += "</div></div></div>";
+  modal.innerHTML = div;
+  modal.style.display = "block";
+}
+
+function handleCloseModal() {
+  const modal = document.getElementById("modalWindow");
+  modal.style.display = "none";
+  modal.innerHTML = "";
 }
 
 function hasClass(elem, className) {
