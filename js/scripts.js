c9f95e8aeea52ec28eb2f29d175eac7f1ee8e178
NikitaSysoev <na.sysoev@gmail.com>
2019-05-10 17:42:34 +0300

    done tasks 1, 2, 4, 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 function test(val) {
-  console.log("This is from JS FILE: " + val);
+  console.log('This is from JS FILE: ' + val);
+}
+
+function isPalindrom(str) {
+  let parseStr = str
+    .toLowerCase()
+    .split(' ')
+    .filter(item => !!item)
+    .join('');
+  return parseStr === [...parseStr].reverse().join('');
 }
